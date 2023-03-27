cda04c1b4adfb0e327219c4809a4c3e7ad274f65
Aureliano <aurelianoclaudio29@gmail.com>
2023-03-27 16:06:34 -0300

    Home page criada

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text("TELA PRINCIPAL"),
+      ),
+    );
+  }
+}
