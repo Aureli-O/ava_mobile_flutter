3436f15d6e7b5fa2a3c33700386868f54d7cbff4
Aureliano <aurelianoclaudio29@gmail.com>
2023-04-12 12:30:24 -0300

    Criação das variáveis Horarios,Frequência e Salas. Adição de um detector de gestos. Mudança no visual do Card.

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -6,46 +6,154 @@ import 'package:google_fonts/google_fonts.dart';
 class MyCard extends StatelessWidget {
   final String NomeCadeira;
   final String NomeProfessor;
+  final String Horarios;
+  final String Frequencia;
+  final String Sala;
 
-  MyCard({required this.NomeCadeira, required this.NomeProfessor});
+  MyCard({
+    required this.NomeCadeira,
+    required this.NomeProfessor,
+    required this.Horarios,
+    required this.Frequencia,
+    required this.Sala,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-      child: Container(
-        height: 170,
-        decoration: BoxDecoration(
-          color: Colors.grey[350],
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.baseline,
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10,10,10,5),
-              child: Text(
-                NomeCadeira,
-                style: GoogleFonts.inter(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[900],
+    return GestureDetector(
+      onTap: () => {},
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+        child: Container(
+          height: 205,
+          decoration: BoxDecoration(
+              color: Colors.grey[350], borderRadius: BorderRadius.circular(15)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 15, 5),
+                child: Text(
+                  NomeCadeira,
+                  style: GoogleFonts.inter(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[900],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10,5,10,5),
-              child: Text(
-                NomeProfessor,
-                style: GoogleFonts.inter(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey[900],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                child: Text(
+                  NomeProfessor,
+                  style: GoogleFonts.inter(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.grey[900],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 0, 0, 10),
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(15),
+                      right: Radius.circular(2),
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 6, 10, 2),
+                            child: Text(
+                              Horarios,
+                              style: GoogleFonts.inter(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[900],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 5),
+                            child: Text(
+                              "Horário",
+                              style: GoogleFonts.inter(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey[900],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 6, 10, 2),
+                            child: Text(
+                              Frequencia,
+                              style: GoogleFonts.inter(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[900],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 6),
+                            child: Text(
+                              "Frequência",
+                              style: GoogleFonts.inter(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey[900],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 6, 10, 2),
+                            child: Text(
+                              Sala,
+                              style: GoogleFonts.inter(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[900],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 5),
+                            child: Text(
+                              "Sala",
+                              style: GoogleFonts.inter(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey[900],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
