776b678091bcf8763f625d57b419b69e81ee233b
Aureliano <aurelianoclaudio29@gmail.com>
2023-04-24 12:15:24 -0300

    Mudança no estilo da tela de perfil

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -16,7 +16,8 @@ class _ProfilePageState extends State<ProfilePage> {
           "https://pps.whatsapp.net/v/t61.24694-24/307108573_462481232600602_3637140898630552775_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdRL9xO_9stemlKkEgvWzgFKG71uomjN8RtXA0SOBwLrlQ&oe=6451AA73",
       name: "Joaozinho da silva pereira",
       email: "joaozinho@unifor.br",
-      about: "Esta é uma pessoa única e interessante. Ela tem uma curiosidade natural pelo mundo e pelas pessoas ao seu redor. Ela valoriza a honestidade, a empatia e o respeito. Além disso, ela está sempre aberta a novas experiências e desafios, e está animada para ver o que o futuro reserva.",
+      about:
+          "Esta é uma pessoa única e interessante. Ela tem uma curiosidade natural pelo mundo e pelas pessoas ao seu redor. Ela valoriza a honestidade, a empatia e o respeito. Além disso, ela está sempre aberta a novas experiências e desafios, e está animada para ver o que o futuro reserva.",
       registry: "2214763-2",
       ongoingCredits: "360",
       completedCredits: "720",
@@ -27,12 +28,13 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: ListView(
         physics: BouncingScrollPhysics(),
         children: [
           SizedBox(height: 12),
           Card(
-            color: Colors.white,
+            color: Colors.grey[350],
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20.0),
             ),
@@ -44,8 +46,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   padding:
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   child: ProfileWidget(
-                    imagePath:
-                        studentM.imagePath,
+                    imagePath: studentM.imagePath,
                     onClicked: () async {},
                   ),
                 ),
@@ -63,8 +64,38 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
+  Widget buildName(Student student) => Column(
+        children: [
+          Text(
+            student.name,
+            style: TextStyle(
+                color: Colors.grey[900],
+                fontWeight: FontWeight.bold,
+                fontSize: 22),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            student.registry,
+            style: TextStyle(color: Colors.grey[600], fontSize: 16),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            student.email,
+            style: TextStyle(color: Colors.grey[600], fontSize: 16),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            student.course,
+            style: TextStyle(
+                color: Colors.grey[900],
+                fontWeight: FontWeight.normal,
+                fontSize: 18),
+          )
+        ],
+      );
+
   Widget cardNumbers(Student student) => Card(
-        color: Colors.white,
+        color: Colors.grey[350],
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20.0),
         ),
@@ -76,54 +107,29 @@ class _ProfilePageState extends State<ProfilePage> {
             children: [
               Text(
                 'PMG: ${student.pmg}',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(color: Colors.grey[900],fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 8.0),
               Text(
                 'Créditos cursados: ${student.completedCredits}',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(color: Colors.grey[900],fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 8.0),
               Text(
                 'Créditos em curso: ${student.ongoingCredits}',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(color: Colors.grey[900],fontWeight: FontWeight.bold),
               ),
             ],
           ),
         ),
       );
-
-  Widget buildName(Student student) => Column(
-        children: [
-          Text(
-            student.name,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            student.registry,
-            style: TextStyle(color: Colors.grey, fontSize: 16),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            student.email,
-            style: TextStyle(color: Colors.grey, fontSize: 16),
-          ),
-          const SizedBox(height: 16),
-          Text(
-            student.course,
-            style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
-          )
-        ],
-      );
-
   // Widget buildUpgradeButton() => ButtonWidget(
   //   text: 'Upgrade To PRO',
   //   onClicked: () {},
   // );
 
   Widget buildAbout(Student student) => Card(
-        color: Colors.white,
+        color: Colors.grey[350],
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20.0),
         ),
@@ -135,12 +141,18 @@ class _ProfilePageState extends State<ProfilePage> {
             children: [
               Text(
                 'Sobre mim',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                style: TextStyle(
+                    color: Colors.grey[900],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24),
               ),
               SizedBox(height: 8.0),
               Text(
                 student.about,
-                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 16),
+                style: TextStyle(
+                    color: Colors.grey[900],
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16),
               ),
             ],
           ),
