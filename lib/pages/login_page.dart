b8e7ca1a55515a44254b3432ccd57f97f2414da1
Aureliano <aurelianoclaudio29@gmail.com>
2023-06-13 07:22:52 -0300

    Adição do Background da tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,6 +15,147 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.asset("lib/assets/loginpageBG.png")),
+          ),
+          
+          SingleChildScrollView(
+            child: Container(
+              //decoração do container
+              margin: EdgeInsets.fromLTRB(
+                  20, MediaQuery.of(context).size.width / 2, 20, 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey[200],
+                shape: BoxShape.rectangle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade800.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                  )
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+
+                  //texto de cima
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Text(
+                      "Universidade de Fortaleza",
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 50,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Text(
+                      "Ensinando e aprendendo",
+                      style: GoogleFonts.robotoCondensed(
+                        fontSize: 24,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+
+                  //matricula textfield //campo de texto do matricula //matricula
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.account_circle_rounded),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 2),
+                            borderRadius: BorderRadius.circular(12)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.indigo, width: 3.5),
+                            borderRadius: BorderRadius.circular(12)),
+                        hintText: 'Matricula',
+                        fillColor: Colors.grey[100],
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+
+                  //Password Textfiel //campo de texto da senha //password
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.visibility_off_rounded),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 2),
+                            borderRadius: BorderRadius.circular(12)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.indigo, width: 3.5),
+                            borderRadius: BorderRadius.circular(12)),
+                        hintText: 'Senha',
+                        fillColor: Colors.grey[100],
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25),
+
+                  //Botão de acesso
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.indigo,
+                        foregroundColor: Colors.grey[200],
+                        fixedSize: Size(150, 50),
+                        textStyle: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text("Acessar"),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  /*
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.grey[200],
       body: SafeArea(
         child: SingleChildScrollView(
@@ -141,4 +282,5 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+  */
 }
